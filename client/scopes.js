8adea5b8d049d611b72fb826b8edbb4079382bff
Asbjørn Steinskog <asbjorn@steinskog.me>
2015-08-07 00:58:12 +0200

    Update scopes

---
+++ client/scopes.js
@@ -0,0 +1,4 @@
+// We need to specify scopes for accessing user-related data
+// https://developer.spotify.com/web-api/using-scopes/
+
+Accounts.ui.config({'requestPermissions':{'spotify': ['playlist-modify-private', 'playlist-modify-public', 'playlist-read-private']}});
